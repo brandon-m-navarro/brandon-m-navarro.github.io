43d19a5c653cbc90b2720d9c0e3e9b7dcf9d719f
brandon-m-navarro <brandon.m.navarro@gmail.com>
2024-07-30 02:06:13 -0400

    Added images

--- js/scripts/Images.js
+++ js/scripts/Images.js
@@ -5,6 +5,38 @@ export default class Images {
             'dadongo': {
                 src: '../../img/Dodongo.jpg',
                 alt: 'Dodongo [oot]'
+            },
+            'email': {
+                src: '../../img/envelope-regular.svg',
+                alt: 'Email'
+            },
+            'location': {
+                src: '../../img/location-dot-solid.svg',
+                alt: 'Location'
+            },
+            'location-2': {
+                src: '../../img/location-dot-solid.svg',
+                alt: 'Location_2'
+            },
+            'website': {
+                src: '../../img/computer-solid.svg',
+                alt: 'Website'
+            },
+            'linkedIn': {
+                src: '../../img/linkedin-brands-solid.svg',
+                alt: 'LinkedIn'
+            },
+            'education': {
+                src: '../../img/school-solid.svg',
+                alt: 'Education'
+            },
+            'calendar': {
+                src: '../../img/calendar-solid.svg',
+                alt: 'Calendar'
+            },
+            'work': {
+                src: '../../img/briefcase-solid.svg',
+                alt: 'Work'
             }
         }
     }
